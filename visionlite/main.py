95bf69684c9c1405097981b9f859ab3d3c542945
santhoshkammari <santhoshkammari1999@gmail.com>
2024-12-22 06:24:06 +0530

    fix vision

--- visionlite/main.py
+++ visionlite/main.py
@@ -12,36 +12,61 @@ def get_llm():
     return _loaded_llm
 
 
+def get_urls(name='google', query=None, max_urls=None, animation=None):
+    _func = google if name == 'google' else bing
+    return _func(query, max_urls=max_urls, animation=animation)
+
 def vision(query, k=1, max_urls=5, animation=False,
            allow_pdf_extraction=True,
            allow_youtube_urls_extraction=False,
-           embed_model=None):
+           embed_model=None,
+           return_only_urls = False,
+           return_with_urls = False):
     try:
-        urls = google(query, max_urls=max_urls, animation=animation)
+        urls = get_urls(
+            name='google',
+            query=query,
+            max_urls=max_urls,
+            animation=animation
+        )
+        if return_only_urls:
+            return urls
         contents = parse(urls, allow_pdf_extraction=allow_pdf_extraction,
                          allow_youtube_urls_extraction=allow_youtube_urls_extraction)
         contents = [_.content for _ in contents]
         llm = embed_model or get_llm()
         res = llm.topk(query, llm.split("".join(contents)), k=k)
-        updated_res = "\n".join(res) + "\n\nURLS:\n" + "\n".join(urls)
+        if return_with_urls:
+            return res, urls
+        return res
     except Exception as e:
         return f"Error: {str(e)}"
-    return updated_res
 
 
 def visionbing(query, k=1, max_urls=5, animation=False,
                allow_pdf_extraction=True,
                allow_youtube_urls_extraction=False,
-               embed_model=None):
+               embed_model=None,
+               return_only_urls=False,
+               return_with_urls=False
+               ):
     try:
-        urls = bing(query, max_urls=max_urls, animation=animation)
+        urls = get_urls(
+            name='bing',
+            query=query,
+            max_urls=max_urls,
+            animation=animation
+        )
+        if return_only_urls:
+            return urls
         contents = parse(urls, allow_pdf_extraction=allow_pdf_extraction,
                          allow_youtube_urls_extraction=allow_youtube_urls_extraction)
         contents = [_.content for _ in contents]
         llm = embed_model or get_llm()
         res = llm.topk(query, llm.split("".join(contents)), k=k)
-        updated_res = "\n".join(res) + "\n\nURLS:\n" + "\n".join(urls)
+        if return_with_urls:
+            return res, urls
+        return res
     except Exception as e:
         return f"Error: {str(e)}"
-    return updated_res
 
